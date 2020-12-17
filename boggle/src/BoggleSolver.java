84e906d8013217a5e7c2cf194ac96b2b90351d78
liang guo <liang.guo@ensto.com>
2020-12-17 23:46:25 +0200

    own

--- boggle/src/BoggleSolver.java
+++ boggle/src/BoggleSolver.java
@@ -1,26 +1,36 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
-
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TST;
+
 
 public class BoggleSolver {
-    private static final int R = 26;      // A-Z
-
-    private boolean[] marked;
-    private char[] board;
-    private int rows;
-    private int cols;
-    private Cube[] adj;
-    private Node root;
+    private static final int[][] directions = {
+            {-1, 0}, // top
+            {-1, 1}, // top right
+            {0, 1}, // right
+            {1, 1}, // bottom right
+            {1, 0}, // bottom
+            {1, -1}, // bottom left
+            {0, -1}, // left
+            {-1, -1} // top left
+    };
+    private int boardRows;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-    public BoggleSolver(String[] dictionary) {
-        if (dictionary == null)
-            throw new IllegalArgumentException("the argument to BoggleSolver() is null\n");
+    private SET<String> validWords;
+    private int boardCols;
+    private int boardCount;
+    private TST dictionaryTrie;
 
-        for (int i = 0; i < dictionary.length; i++)
-            addToTrie(dictionary[i]);
+    public BoggleSolver(String[] dictionary) {
+        validWords = new SET<>();
+        dictionaryTrie = new TST();
+
+        for (String s : dictionary) {
+            dictionaryTrie.put(s, 1);
+        }
     }
 
     public static void main(String[] args) {
@@ -36,153 +46,97 @@ public class BoggleSolver {
         StdOut.println("Score = " + score);
     }
 
-    private void addToTrie(String word) {
-        if (word == null)
-            throw new IllegalArgumentException("argument to addToTrie() is null");
-
-        root = add(root, word, 0);
-    }
-
-    private Node add(Node x, String word, int d) {
-        if (x == null) x = new Node();
-        if (d == word.length()) x.isWord = true;
-        else {
-            char c = word.charAt(d);
-            x.next[c - 'A'] = add(x.next[c - 'A'], word, d + 1);
+    private void dfs(BoggleBoard board, int row, int col, boolean[][] visited, StringBuilder sb) {
+        if (row < 0 || row > boardRows - 1) {
+            return;
         }
-        return x;
-    }
 
-    private boolean contains(String word) {
-        if (word == null)
-            throw new IllegalArgumentException("argument to contains() is null");
+        if (col < 0 || col > boardCols - 1) {
+            return;
+        }
 
-        Node x = get(root, word, 0);
-        if (x == null) return false;
-        return x.isWord;
-    }
 
-    private Node get(Node x, String word, int d) {
-        if (x == null) return null;
-        if (d == word.length()) return x;
-        char c = word.charAt(d);
-        return get(x.next[c - 'A'], word, d + 1);
+        // visit board[row][col]
+        if (visited[row][col]) {
+            return;
+        }
+
+        if (sb.length() > 0) {
+            if (!dictionaryTrie.keysWithPrefix(sb.toString()).iterator().hasNext()) {
+                return;
+            }
+        }
+
+        visited[row][col] = true;
+        char letter = board.getLetter(row, col);
+        if (letter == 'Q') {
+            sb.append(letter);
+            sb.append('U');
+        } else {
+            sb.append(letter);
+        }
+
+
+        if (sb.length() >= 3) {
+            String s = sb.toString();
+            if (dictionaryTrie.contains(s)) {
+                validWords.add(s);
+            }
+        }
+
+        // iterate over 9 directions
+
+        for (int[] direction : directions) {
+            dfs(board, row + direction[0], col + direction[1], visited, sb);
+        }
+
+
+        sb.deleteCharAt(sb.length() - 1);
+        if (sb.length() > 0) {
+            if (sb.charAt(sb.length() - 1) == 'Q') {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+        visited[row][col] = false;
+
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        if (board == null)
-            throw new IllegalArgumentException("the argument to getAllValidWords() is null\n");
+        boardRows = board.rows();
+        boardCols = board.cols();
+        boardCount = boardRows * boardCols;
 
-        if (rows != board.rows() || cols != board.cols()) {
-            rows = board.rows();
-            cols = board.cols();
-            marked = new boolean[rows * cols];
-            this.board = new char[rows * cols];
-            precomputeAdj();
-        }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int idx = i * cols + j;
-                char c = board.getLetter(i, j);
-                this.board[idx] = c;
+        boolean[][] visited = new boolean[boardRows][boardCols];
+
+        for (int row = 0; row < boardRows; row++) {
+            for (int col = 0; col < boardCols; col++) {
+                StringBuilder sb = new StringBuilder();
+                dfs(board, row, col, visited, sb);
             }
         }
 
-        SET<String> allValidWords = DFS();
-        return allValidWords;
+        return validWords;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        if (word == null)
-            throw new IllegalArgumentException("the argument to scoreOf() is null\n");
-
-        if (!contains(word)) return 0;
-        else if (word.length() < 3) return 0;
-        else if (word.length() < 5) return 1;
-        else if (word.length() == 5) return 2;
-        else if (word.length() == 6) return 3;
-        else if (word.length() == 7) return 5;
-        else return 11;
-    }
-
-    private SET<String> DFS() {
-        SET<String> words = new SET<String>();
-        for (int i = 0; i < rows * cols; i++)
-            DFS(i, new StringBuilder(), words, root);
-        return words;
-    }
-
-    private void DFS(int idx, StringBuilder pre, SET<String> words, Node n) {
-        char c = board[idx];
-        Node next = n.next[c - 'A'];
-        if (c == 'Q' && next != null)
-            next = next.next['U' - 'A'];
-        if (next == null) return;
-
-        if (c == 'Q') pre.append("QU");
-        else pre.append(c);
-        String str = pre.toString();
-        if (pre.length() > 2 && next.isWord)
-            words.add(str);
-
-        marked[idx] = true;
-        for (int k = 0; k < adj[idx].n; k++) {
-            int nextIdx = adj[idx].neighbor[k];
-            if (!marked[nextIdx])
-                DFS(nextIdx, new StringBuilder(pre), words, next);
-        }
-        marked[idx] = false;
-    }
-
-    private void precomputeAdj() {
-        adj = new Cube[rows * cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int idx = i * cols + j;
-                adj[idx] = new Cube();
-                if (i > 0) {
-                    // 正上
-                    adj[idx].neighbor[adj[idx].n++] = (i - 1) * cols + j;
-                    // 右上
-                    if (j < cols - 1)
-                        adj[idx].neighbor[adj[idx].n++] = (i - 1) * cols + j + 1;
-                }
-                if (i < rows - 1) {
-                    // 正下
-                    adj[idx].neighbor[adj[idx].n++] = (i + 1) * cols + j;
-                    // 左下
-                    if (j > 0)
-                        adj[idx].neighbor[adj[idx].n++] = (i + 1) * cols + j - 1;
-                }
-                if (j > 0) {
-                    // 左边
-                    adj[idx].neighbor[adj[idx].n++] = i * cols + j - 1;
-                    // 左上
-                    if (i > 0)
-                        adj[idx].neighbor[adj[idx].n++] = (i - 1) * cols + j - 1;
-                }
-                if (j < cols - 1) {
-                    // 右边
-                    adj[idx].neighbor[adj[idx].n++] = i * cols + j + 1;
-                    // 右下
-                    if (i < rows - 1)
-                        adj[idx].neighbor[adj[idx].n++] = (i + 1) * cols + j + 1;
-                }
+        int score = 0;
+        if (dictionaryTrie.contains(word)) {
+            int wordLength = word.length();
+            if (wordLength >= 3 && wordLength <= 4) {
+                score = 1;
+            } else if (wordLength == 5) {
+                score = 2;
+            } else if (wordLength == 6) {
+                score = 3;
+            } else if (wordLength == 7) {
+                score = 5;
+            } else if (wordLength >= 8) {
+                score = 11;
             }
         }
+        return score;
     }
-
-    private static class Node {
-        private boolean isWord;
-        private Node[] next = new Node[R];
-    }
-
-    private static class Cube {
-        private int n = 0;
-        private int[] neighbor = new int[8];
-    }
-
 }
